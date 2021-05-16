d195f7fbd3a20e6eb488432ba80423c209e98a2f
HossamSayed99 <hossamlion17@gmail.com>
2021-05-16 16:14:49 +0300

    Added optimizers. Solved Reloading issue by re-enabling server worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const {GenerateSW} = require('workbox-webpack-plugin');
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
@@ -13,21 +15,9 @@ module.exports = {
             },
             {
                 test : /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
         ]
-            // TODO 1: Add babel Loader that match js files as development
-            // TODO 2: Add Loaders for
-            //    1. converting sass => css
-            //    2. Turns css into commonjs
-            //    3. Extract css into files
-            /* HINT: structure
-        {
-          test: REGEX_TO_MATCH_FILES ex. /\.js$/,
-          exclude: /node_modules/,
-          loader: '',
-        }
-       */
     },
     output :{
         libraryTarget: 'var',
@@ -39,9 +29,9 @@ module.exports = {
             filename: './index.html'
         }),
         new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
-        // TODO: configure workbox-webpack-plugin
+        new GenerateSW()
     ],
     optimization: {
-        // TODO: Add Optimization for JS and CSS
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
     },
 }
